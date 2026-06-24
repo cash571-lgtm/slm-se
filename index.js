0ea710b681175e74b6acffb737106feb0efbb356
cash571-lgtm <zaincash571@gmail.com>
2026-06-24 21:00:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -513,7 +513,7 @@ io.on("connection", (socket) => {
 });
 
 mongoose
-  .connect("mongodb+srv://abshr:abshr@abshr.fxznc.mongodb.net/SalamaTest")
+  .connect("mongodb+srv://salamav1:LHVkGYICf0uo1P3U@cluster0.emazq5u.mongodb.net/salama")
   .then((conn) =>
     server.listen(PORT, async () => {
       // await Order.deleteMany({})
